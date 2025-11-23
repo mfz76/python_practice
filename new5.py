16d639d3fce8d08c95839ed40ba5e92cd18e2bbd
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-11-23 23:19:02 +0530

    Add files via upload

---
+++ new5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  2 16:25:37 2025
+
+@author: MOHAMMAD FARAx
+"""
+x = input("Enter the verb you wanna enter: ")
+print("i can" ,x,"better than you.")
+
+print((x + " ") *4 + "do")
+
