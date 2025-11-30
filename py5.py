cf3e9d70901e0996388e37035c9a8c6d714baf1a
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-11-30 23:59:22 +0530

    Add files via upload

---
+++ py5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  4 13:17:22 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+num1 = int(input("Enter the first figure:"))
+num2 = int(input("Enter the second figure:"))
+
+sum = num1 + num2
+sub = num1 + num2
+multi = num1 * num2
+div = num1/num2
+div_round = round(div,2)
+
+print("Sumation:",sum)
+print("Subtraction:",sub)
+print("Multiplication:",multi)
+print("Quotient:",div_round)
