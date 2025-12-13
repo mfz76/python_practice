99e9800ca8ec0948bc7352a67d76a724273d6c7a
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-13 23:55:19 +0530

    Add files via upload

---
+++ py8.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  4 13:50:29 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+'''i = 0;total = 0
+for i in range(1,20):
+    print(i)
+    total += i
+print("sum:",total)'''
+
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+print(total)
