7333750e9955dc9279222529cb0d21c227549202
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-14 22:39:33 +0530

    Add files via upload

---
+++ py7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  4 13:40:08 2025
+
+@author: MOHAMMAD FARAZ
+"""
+'''i = 0
+while(i<5):
+   print(i)
+   i += 1'''
+   
+i=0
+for i in range(1,20,2):
+    print(i)
