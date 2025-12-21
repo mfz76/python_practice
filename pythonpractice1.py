2687276005bb07d066c46035d7a6cce3f4f01db7
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-21 23:51:49 +0530

    Add files via upload

---
+++ pythonpractice1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 15 15:00:23 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+
+'''t = "i will win "
+y = t[1:len(t)]
+print(y)'''
+
+'''a = "the"
+b = "three"
+c = "musketeers"
+print(a,b,c)
+'''
+
+'''text = input("type anything:")
+print(text)'''
+
+
+variable = input("what is your name?\n")
+print(f"Hey, {variable}, how have you been till now, "
+      " you brought the sunshine.")
