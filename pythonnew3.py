42139453772bb169906bb5ff0b8439c88ff8e1a2
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-25 23:51:50 +0530

    Add files via upload

---
+++ pythonnew3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 15 15:47:53 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+n = int(input("Enter a non-negative integer."))
+
+while n>0:
+    print("x")
+n= n-1
