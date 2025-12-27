8ef6954456322c570be2126f895a2aa8c63c6be0
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-27 16:05:50 +0530

    Add files via upload

---
+++ printthewinner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 27 16:00:13 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+n1 = int(input("Enter the number:\n"))
+n2 = int(input("Enter the number:\n"))
+
+if (n1<n2):
+    print(f"Hence, the largest number is {n2}")
+else :
+    print(f"Hence, the largest number is {n1}")
