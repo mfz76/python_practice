b402e331f68dca1f3ab31f4e1465c7913fd17b35
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-27 15:57:02 +0530

    Add files via upload

---
+++ pos.neg.inpython.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 27 15:51:22 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+n1 = int(input("Enter the number:\n"))
+if (n1>0):
+     print("its positive")
+elif (n1<0):
+     print("Its negative")
+else :
+     print("its nope")
+     
+ 
