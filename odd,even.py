3f760338a223da53179e8bed07833582ce46af92
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-27 15:49:47 +0530

    Add files via upload

---
+++ odd,even.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 27 14:31:26 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+variable =int(input("Enter the number you wanna check for:\n"))
+if (variable == 0):
+    print("its zero")
+elif(variable%2==0):
+    print("even")
+else:
+    print("odd")
+
+    
