a41708b16f19d0835c920bb9d04f8fb07f3fbcae
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-27 15:49:29 +0530

    Add files via upload

---
+++ greaterthan.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 27 15:30:14 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+x1 = int(input("give x1 the numbers\n"))
+x2 = int(input("give x2 the numbers\n"))
+if (x1>x2):
+    print("x1 is greater")
+elif (x1<x2):
+    print("x2 is greater")
+else:
+    print("Equaltiy established")
+    
+         
+         
