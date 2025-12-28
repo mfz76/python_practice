3dcd4be2d3cd999a924a713a12d705533f8e2fe5
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-28 19:50:21 +0530

    Add files via upload

---
+++ newj.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 28 19:46:03 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+x = input("Whats your name?\n")
+print(f"how are you,{x}! Hope you are doing good.")
