5624daf6fc829f5e93343216f6838ddf500dd151
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-28 19:57:27 +0530

    Add files via upload

---
+++ nj2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 28 19:51:41 2025
+
+@author: MOHAMMAD FARAZ
+"""
+i = int(input("enter the first number:"))
+j = int(input("enter the second number:"))
+l = i+j
+print(l)
