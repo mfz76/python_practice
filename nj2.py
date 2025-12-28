a56a57e04b51f3d42a5c0bf6d4c3de3f4ef6ad69
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-28 20:51:38 +0530

    Add files via upload

--- nj2.py
+++ nj2.py
@@ -4,7 +4,12 @@ Created on Sun Dec 28 19:51:41 2025
 
 @author: MOHAMMAD FARAZ
 """
-i = int(input("enter the first number:"))
-j = int(input("enter the second number:"))
-l = i+j
-print(l)
+x = int(input("Enter the marks:\n"))
+if ( x>=90  and x<=100 ) :
+    print("A")
+elif (x>=75 and x<90 ):
+    print("B")
+elif (x>=60 and x<75):
+    print("c")
+else :
+    print("you are fail mother fucker get the hell out of here")
