869bbda2f5186a792e6a158857a8587e0a6e0827
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-29 19:22:39 +0530

    Add files via upload

---
+++ looping3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 29 19:13:27 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+n= int(input("Enter the number of which you wanna table:"))
+for j in range(0,11):
+   input = n *  j 
+   print(f"{n}*{j}= {input}")
