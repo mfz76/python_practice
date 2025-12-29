096aa3f9a1166fa6e118def12a236b6024c11b0b
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-29 19:11:29 +0530

    Add files via upload

---
+++ looping2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 29 18:58:17 2025
+
+@author: MOHAMMAD FARAZ
+"""
+sum = 0
+n = int(input("Enter the amount of numbers you wanna input:"))
+for i in  range(0,n+1):
+    sum = i + sum
+print(sum)
