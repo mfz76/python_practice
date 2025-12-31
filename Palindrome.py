e90411533ff01d8c05d5b8d76d0de60ee2474f4f
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2025-12-31 23:15:31 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 31 23:06:49 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+py = input("Enter something:").strip()
+if (py == py[::-1]):
+    print("Yes, its a Palindrome.")
+else:
+    print("No")
