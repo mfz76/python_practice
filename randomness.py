900ca4a62c79f38ea7653763aeef221451079b96
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-01 22:06:52 +0530

    Add files via upload

---
+++ randomness.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  1 21:53:17 2026
+
+@author: MOHAMMAD FARAZ
+"""
+import random
+y = random.randint(0,10)
+x = int(input("Enter the number:"))
+if (y == x ):
+    print("Same")
+else:
+    print("fuck off")
