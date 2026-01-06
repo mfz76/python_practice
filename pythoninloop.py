5e5145dd14b92a229e43d316c5679fbfd29c69bf
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-06 15:47:51 +0530

    Add files via upload

---
+++ pythoninloop.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  6 15:42:51 2026
+
+@author: MOHAMMAD FARAZ
+"""
+
+n = int(input("Enter the number which is non-negative except 0:"))
+while (n>0):
+    print("x")
+    n = n - 1
