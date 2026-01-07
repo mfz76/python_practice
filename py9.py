02b85c53ffae716b92ba2bf522b6600c00118c8e
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-07 23:56:58 +0530

    Add files via upload

---
+++ py9.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 08:02:40 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+a = "the"
+b = 3
+c = "musketeers"
+
+#print(a,b,c)
+
+
+print(a+str(b)+c)
+
+
+
+
