0e6a766c81528dc6c2b3c26b969155675f6b4e51
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-09 23:55:13 +0530

    Add files via upload

---
+++ newpython2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 15 15:30:15 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+'''num1 = input("Enter the number:")
+print(5*num1)'''
+
+num1 = input("Enter the number:")
+print(num1*5)
