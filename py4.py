71c964c381b53b2631654f8a5c35f1f21e26c976
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-09 23:50:29 +0530

    Add files via upload

---
+++ py4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  4 13:10:55 2025
+
+@author: MOHAMMAD FARAZ
+"""
+
+a = 10 
+b = 25
+  
+sum = a + b 
+sub  = a - b
+multi = a*b
+division = a/b
+print("add=",sum)
+print("sub=",sub)
+print("mutli=",multi)
+print("div=",division)
