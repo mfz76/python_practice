fb8fd4b703d4fca5bb5beab113c76c4a3a3fc699
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-14 23:04:02 +0530

    Add files via upload

---
+++ finance.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 14 22:21:19 2026
+
+@author: MOHAMMAD FARAZ
+"""
+
+yearly_salary = float(input("Enter the yearly salary:\n"))
+portion_saved = float(input("Enter the portion saved of the salary.\n"))
+cost_of_dream_home = float(input("Enter the cost of your dream home.\n"))
+number_of_months =  int(input("Enter the number of months:\n"))
