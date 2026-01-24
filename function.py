d7d957d0f6cad485be78155864dc7673dcd94488
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-24 18:35:55 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 24 18:08:38 2026
+
+@author: MOHAMMAD FARAZ
+"""
+
+def is_even(i):
+    return i%2 == 0
+        
+        
+var=is_even(100)
+print(var)
