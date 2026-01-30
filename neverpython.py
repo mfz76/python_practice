0b6bb958e24856f6ffbb335fe04c62f15460b175
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-30 23:58:46 +0530

    Add files via upload

---
+++ neverpython.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 30 16:00:31 2026
+
+@author: MOHAMMAD FARAZ
+"""
+
+a = int(input("Enter the first Input/integer:\n"))
+b = int(input("Enter the second Input/integer:\n"))
+print(a+b)
+print(a-b)
+print(ab)
