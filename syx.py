c7c50b6003db2bfa955a425853242ecf93e306db
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-01-31 23:53:00 +0530

    Add files via upload

---
+++ syx.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 30 09:47:41 2026
+
+@author: MOHAMMAD FARAZ
+"""
+count = 0
+s = "a,e,i,o,u"
+v = "how are you?"
+for i in range(len(V)):
+    if i == s:
+        count += 1
