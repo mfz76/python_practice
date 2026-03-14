843439ddc4975fd7757952a9beab369688e68c5e
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-03-14 15:50:03 +0530

    Add files via upload

---
+++ executionflow.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 13:33:49 2026
+
+@author: MOHAMMAD FARAZ
+"""
+
+
+def repeatlyrics():
+    printlyrics()
+def printlyrics():
+    print("Hey, i am here.\n")
+    
+repeatlyrics()
