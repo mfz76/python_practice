5a15837d803da4bfe027824e1314d6ad78121c16
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-07-04 23:58:59 +0530

    Add files via upload

---
+++ praci.py
@@ -0,0 +1,27 @@
+#second largest digit of the number
+
+'''x = str(abs(int(input("Enter the number:"))))
+
+largest = 0
+seclargest = -1
+
+for  ch in x:
+    if   "0" <=    ch   <= "9":
+        digit = int(ch)
+        if digit > largest:
+            largest = digit
+    
+
+            
+for ch in x:
+    if "0" <= ch <="9":
+        digit = int(ch)
+        if digit < largest and digit > seclargest:
+            seclargest = digit
+        
+
+print(seclargest)'''
+            
+
+    
+            
