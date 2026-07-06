06935e3ebdf2ad4ad1952f64478c33aa1779c274
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-07-06 22:41:57 +0530

    Add files via upload

--- pracv.py
+++ pracv.py
@@ -1,3 +1,26 @@
 #check whether string contains any uppercase letter or not
 
 
+while True:
+    try:
+        x = input("Enter the word:").strip()
+        break
+    except Exception:
+        print("something is wrong!")
+
+Noupper = True
+
+for ch in x:
+    if   "A" <= ch  <= "Z":
+        Noupper = False
+        break
+
+if Noupper:
+    print("there is no any uppercase letter.")
+else:
+    print("there is an uppercase letter.")
+    
+
+
+
+
